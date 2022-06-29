0a32815517442c610c75a0d0501729c2e6ca87c6
vilvadot <hi@vilva.io>
2022-06-29 19:41:30 +0100

    Adjust styles

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import { COLORS } from "./tiles.js";
 
 const fontSize = 20;
-const width = fontSize * 3;
-const height = fontSize * 2;
+const width = fontSize * 2.5;
+const height = fontSize * 1.8;
 
 export const OPTIONS = {
   bg: COLORS.background,
