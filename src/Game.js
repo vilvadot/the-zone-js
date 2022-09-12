cc46fd74ca348427cab8a814772e8547fcbd87ad
vilvadot <hi@vilva.io>
2022-09-12 21:01:26 +0200

    [WIP] Travel between areas

--- src/Game.js
+++ src/Game.js
@@ -19,6 +19,39 @@ import { UIRendering } from "./ui/system.js";
 import { initializeEntityDebugSystem } from "./debug.js";
 import { Corpse } from "./entities/index.js";
 import { Logger } from "./Logger.js";
+import { HEIGHT, WIDTH } from "./config.js";
+
+class Travel {
+  static run(entities, world, display) {
+    for (const { id, sprite, position, velocity, isPlayer } of entities) {
+      if (!isPlayer) continue;
+      if (position.x === 0 && velocity.x === -1) {
+        world.travelWest();
+        position.x = WIDTH;
+        display.clear();
+      }
+
+      if (position.x === WIDTH - 1 && velocity.x === 1) {
+        world.travelEast();
+        position.x = -1;
+        display.clear();
+      }
+
+        console.log(velocity)
+      if (position.y === HEIGHT - 1 && velocity.y === 1) {
+        world.travelSouth();
+        position.y = -1;
+        display.clear();
+      }
+
+      if (position.y === 0 && velocity.y === -1) {
+        world.travelNorth();
+        position.y = HEIGHT;
+        display.clear();
+      }
+    }
+  }
+}
 
 export class Game {
   constructor(bus, display, world, entities) {
@@ -36,19 +69,21 @@ export class Game {
   }
 
   runMainLoop() {
-    Spawn.run(this.entities, this.world);
     this.world.generate();  
+    Spawn.run(this.entities, this.world);
     Rendering.run(this.entities);
     this.ui.update(this.entities, this.turn);
-    WorldRendering.run(this.world, this.display, this.player)
+    WorldRendering.run(this.world, this.display, this.player);
 
     this.bus.subscribe(EVENTS.TURN_PASSED, (action) => {
+      this.world.generate();
       this.turn++;
       KeyboardControl.run(this.entities, action);
+      Travel.run(this.entities, this.world, this.display);
+      Movement.run(this.entities, this.world);
       Pickup.run(this.logger, this.entities, action);
       Targetting.run(this.entities, action);
       Pathfinding.run(this.entities, this.world);
-      Movement.run(this.entities, this.world);
       Collision.run(this.entities);
       Combat.run(this.bus, this.logger, this.entities);
       Animation.run(this.entities);
--- src/World/World.js
+++ src/World/World.js
@@ -1,58 +1,94 @@
-import { TILES } from '../tiles.js'
-import { roll } from '../util.js'
+import { TILES } from "../tiles.js";
+import { randomInteger } from "../util.js";
 
 export class World {
   constructor(bus, display, map, generator) {
-    this.bus = bus
-    this.display = display
-    this.map = map
-    this.generator = generator
-  } 
-
-  get width(){
-    return this.map.width
+    this.bus = bus;
+    this.display = display;
+    this.map = map;
+    this.generator = generator;
+    this.currentArea = [0, 0];
+    this.areas = {
+      "0,0": "1233",
+    };
   }
 
-  get height(){
-    return this.map.height
+  get width() {
+    return this.map.width;
+  }
+
+  get height() {
+    return this.map.height;
+  }
+
+  travelWest() {
+    this.currentArea[0]--;
+    this.generate()
+  }
+
+  travelEast() {
+    this.currentArea[0]++;
+    this.generate()
+  }
+
+  travelNorth() {
+    this.currentArea[1]++;
+    this.generate()
+  }
+
+  travelSouth() {
+    this.currentArea[1]--;
+    this.generate()
+  }
+
+  _getCurrentArea() {
+    const id = this.currentArea.join(",");
+    const result = this.areas[id];
+
+    if (!result) this.areas[id] = `${randomInteger(0, 99999)}`;
+
+    return this.areas[id];
   }
 
   generate() {
+    const seed = this._getCurrentArea();
+
     this.generator
-      .generate((x, y, isFilled) => {
-        const tile = isFilled ? TILES.wall : TILES.empty
-        this.map.add(x, y, tile)
-      })
+    .setSeed(seed)
+    .generate((x, y, isFilled) => {
+      const tile = isFilled ? TILES.wall : TILES.empty;
+      this.map.add(x, y, tile);
+    });
   }
 
-  addWall(x,y){
-    this.map.add(x,y, TILES.wall)
-    return this
+  addWall(x, y) {
+    this.map.add(x, y, TILES.wall);
+    return this;
   }
 
-  getCenter(){
+  getCenter() {
     return {
       x: Math.floor(this.width / 2),
-      y: Math.floor(this.height / 2) 
-    }
+      y: Math.floor(this.height / 2),
+    };
   }
 
-  isBlocked(x, y){
-    const tile = this.map.getTile(x,y)
-    if (!tile) return true
+  isBlocked(x, y) {
+    const tile = this.map.getTile(x, y);
+    if (!tile) return true;
 
-    const isFree = tile === TILES.empty
-    return !isFree
+    const isFree = tile === TILES.empty;
+    return !isFree;
   }
 
-  getTileAt(x,y){
-    const tile = this.map.getTile(x,y)
+  getTileAt(x, y) {
+    const tile = this.map.getTile(x, y);
     return tile;
   }
 
-  getRandomFreeCell(){
-    let tile = this.map.getRandomCellCoordinates()
-    if(this.isBlocked(tile.x, tile.y)) return this.getRandomFreeCell()
-    return tile
+  getRandomFreeCell() {
+    let tile = this.map.getRandomCellCoordinates();
+    if (this.isBlocked(tile.x, tile.y)) return this.getRandomFreeCell();
+    return tile;
   }
-};
+}
