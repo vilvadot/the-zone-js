9c08a494115b9ccee1514d8b92642cd2b846ded7
vilvadot <hi@vilva.io>
2022-09-12 20:49:22 +0200

    Dont persist viewed tiles

--- src/systems/WorldRendering.js
+++ src/systems/WorldRendering.js
@@ -6,27 +6,28 @@ const VIEW_RADIUS = 30;
 
 export class WorldRendering {
   static run(world, display, player) {
+    display.clear()
     const fov = new ROT.FOV.PreciseShadowcasting((x, y) => {
       if (world.isBlocked(x, y)) return false;
       return true;
     });
 
-    fov.compute(
-      player.position.x,
-      player.position.y,
-      VIEW_RADIUS,
-      (x, y, distance, visibility) => {
-        const value = world.getTileAt(x, y);
-        const tinting = `rgba(0,0,0, ${shadow(distance)}`;
+      fov.compute(
+        player.position.x,
+        player.position.y,
+        VIEW_RADIUS,
+        (x, y, distance, visibility) => {
+          const value = world.getTileAt(x, y);
+          const tinting = `rgba(0,0,0, ${shadow(distance)}`;
 
-        display.draw(x, y, value, tinting);
-      }
-    );
-  }
+          display.draw(x, y, value, tinting);
+        }
+      );
+    }
 }
 
 const shadow = (distance) => {
-  const shadowing = 1 / distance
+  const shadowing = 1 / distance;
   // Make a scale and map it linearly
   return 1 - shadowing;
 };
