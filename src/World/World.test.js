61a8d8befa6c3befa7d00914ebb42a444a7d6b17
vilvadot <hi@vilva.io>
2022-09-18 18:43:21 +0200

    Fix world tests

--- src/World/World.test.js
+++ src/World/World.test.js
@@ -1,12 +1,10 @@
 import { World } from "./World";
-import { Grid } from "./Grid";
-import { Bus } from "../events";
 
 describe("World", () => {
   it("knows its center", () => {
     const width = 20;
     const height = 20;
-    const world = createWorld({ map: new Grid(width, height) });
+    const world = new World(width, height);
 
     const center = world.getCenter();
 
@@ -15,7 +13,7 @@ describe("World", () => {
   });
 
   it("knows if a cell is blocked", () => {
-    const world = createWorld();
+    const world = new World(1, 1);
 
     world.addWall(0, 0);
 
@@ -23,19 +21,19 @@ describe("World", () => {
   });
 
   it("knows where the world ends", () => {
-    const world = createWorld({ map: new Grid(1, 1) });
+    const world = new World(1, 1);
 
     expect(world.isBlocked(3, 3)).toBe(true);
   });
 
   it("knows if a cell is free", () => {
-    const world = createWorld({ map: new Grid(1, 1) });
+    const world = new World(1, 1);
 
     expect(world.isBlocked(0, 0)).toBe(false);
   });
 
   it("[recursive] provides free tiles", () => {
-    const world = createWorld({ map: new Grid(3, 3) });
+    const world = new World(3, 3);
     world.addWall(0, 0);
 
     const { x, y } = world.getRandomFreeCell();
@@ -43,19 +41,3 @@ describe("World", () => {
     expect(world.isBlocked(x, y)).toBe(false);
   });
 });
-
-const createWorld = ({
-  width = 1,
-  height = 1,
-  map = new Grid(width, height),
-} = {}) => {
-  class GeneratorStub {}
-  const displaystub = () => {
-    return {
-      clear: jest.fn(),
-      draw: jest.fn(),
-    };
-  };
-
-  return new World(new Bus(), displaystub(), map, new GeneratorStub());
-};
