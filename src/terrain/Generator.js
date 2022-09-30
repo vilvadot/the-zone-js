2216ee25d1378a2be179997f82da8b6d8858ea95
vilvadot <hi@vilva.io>
2022-09-30 17:36:39 +0200

    Abstract generators

--- src/terrain/Generator.js
+++ src/terrain/Generator.js
@@ -4,9 +4,11 @@ import { TILES } from "../tiles.js";
 
 export class Generator {
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.engine = new ROT.Map.Cellular(width, height);
+    this.generators = [
+      new DirtGenerator(width, height),
+      new WallGenerator(width, height),
+      new GrassGenerator(width, height),
+    ];
   }
 
   setSeed(seed) {
@@ -16,39 +18,57 @@ export class Generator {
 
   generate() {
     let result = new Matrix();
-    result = this._generateBasicMap(result);
-    result = this._addWalls(result);
-    result = this._addGrass(result);
+    this.generators.forEach((generator) => generator.run(result));
 
     return result;
   }
+}
 
-  _addWalls(result) {
-    return this._generate(result, 0.25, (isFilled, originalTile) =>
-      isFilled ? TILES.wall : originalTile
-    );
-  }
-
-
-  _addGrass(result) {
-    return this._generate(result, 0.25, (isFilled, originalTile) =>
-      isFilled ? TILES.grass : originalTile
-    );
-  }
-
-  _generateBasicMap(result) {
-    return this._generate(result, 0.5, (isFilled) =>
-      isFilled ? TILES.dirt : TILES.empty
-    );
-  }
-
-  _generate(result, fullness = 0.5, tileSelectorCallback) {
+class CellularGenerator {
+  generate(result, fullness = 0.5, tileSelectorCallback) {
     this.engine.randomize(fullness).create((x, y, isFilled) => {
-      const originalTile = result.getValue(x,y)
+      const originalTile = result.getValue(x, y);
       const tile = tileSelectorCallback(isFilled, originalTile);
       result.setValue(x, y, tile);
     });
+  }
+}
 
-    return result;
+class DirtGenerator extends CellularGenerator {
+  constructor(width, height) {
+    super();
+    this.engine = new ROT.Map.Cellular(width, height);
+  }
+
+  run(result) {
+    return super.generate(result, 0.5, (isFilled) =>
+      isFilled ? TILES.dirt : TILES.empty
+    );
+  }
+}
+
+class WallGenerator extends CellularGenerator {
+  constructor(width, height) {
+    super()
+    this.engine = new ROT.Map.Cellular(width, height);
+  }
+
+  run(result) {
+    return super.generate(result, 0.25, (isFilled, originalTile) =>
+      isFilled ? TILES.wall : originalTile
+    );
+  }
+}
+
+class GrassGenerator extends CellularGenerator {
+  constructor(width, height) {
+    super()
+    this.engine = new ROT.Map.Cellular(width, height);
+  }
+
+  run(result) {
+    return super.generate(result, 0.2, (isFilled, originalTile) =>
+      isFilled ? TILES.grass : originalTile
+    );
   }
 }
