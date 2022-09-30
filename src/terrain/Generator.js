c625f3a9c78f67947e1d0932ec07d66e0cb18f88
vilvadot <hi@vilva.io>
2022-09-30 17:17:41 +0200

    Extract common generator logic

--- src/terrain/Generator.js
+++ src/terrain/Generator.js
@@ -24,28 +24,28 @@ export class Generator {
   }
 
   _addWalls(result) {
-    this.engine.randomize(0.25).create((x, y, isFilled) => {
-      const originalTile = result.getValue(x, y);
-      const value = isFilled ? TILES.wall : originalTile;
-      result.setValue(x, y, value);
-    });
-
-    return result;
+    return this._generate(result, 0.25, (isFilled, originalTile) =>
+      isFilled ? TILES.wall : originalTile
+    );
   }
 
+
   _addGrass(result) {
-    this.engine.randomize(0.25).create((x, y, isFilled) => {
-      const originalTile = result.getValue(x, y);
-      const value = isFilled ? TILES.grass : originalTile;
-      result.setValue(x, y, value);
-    });
-    
-    return result;
+    return this._generate(result, 0.25, (isFilled, originalTile) =>
+      isFilled ? TILES.grass : originalTile
+    );
   }
 
   _generateBasicMap(result) {
-    this.engine.randomize(0.5).create((x, y, isFilled) => {
-      const tile = isFilled ? TILES.dirt : TILES.empty;
+    return this._generate(result, 0.5, (isFilled) =>
+      isFilled ? TILES.dirt : TILES.empty
+    );
+  }
+
+  _generate(result, fullness = 0.5, tileSelectorCallback) {
+    this.engine.randomize(fullness).create((x, y, isFilled) => {
+      const originalTile = result.getValue(x,y)
+      const tile = tileSelectorCallback(isFilled, originalTile);
       result.setValue(x, y, tile);
     });
 
