f6bb0e9de845921b0dff129bdae4f5379603918b
vilvadot <hi@vilva.io>
2022-10-20 16:00:19 +0200

    Simplify output dir

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-export default defineConfig({
-    test: {
-        exclude: ["src/dist"],
-        environment: "jsdom",
-        include: ['**/*.test.ts'],
-        reporters: 'verbose'
-    },
-});
