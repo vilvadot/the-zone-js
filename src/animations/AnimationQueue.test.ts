4088523f7d679c3a6b1666a600604695e9d4e60a
vilvadot <hi@vilva.io>
2022-11-13 18:24:04 +0100

    Test AnimationQueue

---
+++ src/animations/AnimationQueue.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+import { AnimationQueue } from "./AnimationQueue";
+import { HitAnimation } from "./animations";
+
+describe("AnimationQueue", () => {
+  it("provides next animation frame", () => {
+    const queue = new AnimationQueue();
+    const hitAtOrigin = new HitAnimation(0, 0);
+    queue.add(hitAtOrigin);
+
+    const nextFrame = queue.composeNextFrame();
+
+    expect(nextFrame.getValue(0,0)).toBeDefined()
+  });
+
+  it("combines multiple animations", () => {
+    const queue = new AnimationQueue();
+    const hitAtOrigin = new HitAnimation(0, 0);
+    const hitAtRight = new HitAnimation(1, 0);
+    queue.add(hitAtOrigin);
+    queue.add(hitAtRight);
+
+    const nextFrame = queue.composeNextFrame();
+
+    expect(nextFrame.getValue(0,0)).toBeDefined()
+    expect(nextFrame.getValue(1,0)).toBeDefined()
+    expect(nextFrame.getValue(2,0)).toBeUndefined()
+  });
+});
--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -1,11 +1,12 @@
+import { Animation } from "./Animation";
 import { Matrix } from "../data-structures/Matrix";
 import { TILES } from "../tiles";
 
 export class HitAnimation extends Animation {
-    frames: Matrix[];
-  
-    constructor(x: number, y: number) {
-      super()
-      this.frames = [new Matrix().setValue(x, y, TILES.hit)];
-    }
+  frames: Matrix[];
+
+  constructor(x: number, y: number) {
+    super();
+    this.frames = [new Matrix().setValue(x, y, TILES.hit)];
   }
+}
